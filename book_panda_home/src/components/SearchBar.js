d93626a2177ded578c4c48179e9269d9517b1ebb
wooseok <gowoo97@gmail.com>
2024-06-09 20:39:12 +0900

    feat: add autocomplete

--- book_panda_home/src/components/SearchBar.js
+++ book_panda_home/src/components/SearchBar.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import {React,useEffect,useState} from 'react';
 import styles from '../styles/SearchBar.module.css';
+import axios from 'axios';
 
 function SearchBar(){
+
+    const [autocompleteItem, setAutocompleteItem] = useState([]);
+    const [keyword, setKeyword] = useState("");
+    const [toggle,setToggle] = useState(false);
+
+    const keywordHandler = (e)=>{
+        setKeyword(e.target.value);
+        setToggle(true);
+    }
+
+    const clickClose = (e)=>{
+        setToggle(false);
+    }
+
+    useEffect(()=>{
+
+        axios.get("http://localhost:8080/bookSales/title?keyword="+keyword)
+        .then((res)=>{
+            setAutocompleteItem(res.data);
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+
+    },[keyword])
+
     return(
         <div className={styles.container}>
             <h1>책판다</h1>
@@ -11,9 +38,21 @@ function SearchBar(){
                     <option>국내도서</option>
                     <option>외국도서</option>    
                 </select>      
-                <input type='text'></input>  
+                <input type='text' onChange={keywordHandler}></input>  
                 <button type='submit'>검색</button>
             </div>    
+            <div className={toggle ? `${styles.autocompleteVisible}`:`${styles.autocompleteHidden}`}>
+                <div className={styles.itemContainer}>
+                   {
+                    autocompleteItem.map((bookTitle,i)=>
+                        <div key={i} className={styles.autocompleteItem}>
+                            {bookTitle.title}
+                        </div>
+                    )
+                   }
+                   <button onClick={clickClose}>닫기</button>
+                </div>
+            </div>
         </div>
     )
 }
