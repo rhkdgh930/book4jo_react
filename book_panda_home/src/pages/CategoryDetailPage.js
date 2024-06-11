76312e39fb5f3f141da02860bda0c2010dc1fbea
wooseok <gowoo97@gmail.com>
2024-06-11 15:47:59 +0900

    feat: add order books

--- book_panda_home/src/pages/CategoryDetailPage.js
+++ book_panda_home/src/pages/CategoryDetailPage.js
@@ -10,6 +10,7 @@ function CategoryDetailPage(){
     const [categoryName,setCategoryName] = useState("");
     const [searchParams, setSearchParams] = useSearchParams();
     const [bookSalesList, setBookSalesList] = useState([]);
+    const [order,setOrder] = useState("");
     const [page,setPage] = useState(1);
 
     useEffect(()=>{
@@ -23,24 +24,39 @@ function CategoryDetailPage(){
         });
 
 
-        axios.get('http://localhost:8080/bookSales?categoryId='+searchParams.get('id')+'&page='+(page-1)+'&size=10')
+        axios.get('http://localhost:8080/bookSales?categoryId='+searchParams.get('id')+'&page='+(page-1)+'&size=10'+'&order='+order)
         .then((res)=>{
             setBookSalesList(res.data);
-            
+            console.log(res.data);
         })
 
 
 
-    },[searchParams,page]);
+    },[searchParams,page,order]);
+
 
     const handlePageChange = (page) =>{
         setPage(page);
         
     };
 
+
+    const handleOrderStat = (e) => {
+        console.log(e.target.value);
+        setOrder(e.target.value);
+        setPage(1);
+    }
+
     return(
         <div className={styles.container}>
+
             <h1>{categoryName}</h1>
+            
+            <select name="order" onChange={handleOrderStat}>
+                <option value="">등록순</option>
+                <option value="sellCount">판매량순</option>
+                <option value="visitCount">조회순</option>
+            </select>
             <div className={styles.books}>
             {
                 bookSalesList.map((bookSales,i)=>
